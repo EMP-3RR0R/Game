f3a5696b4d95d0d311de5b363fe5f2a68ba8109d
EMP-3RR0R <andrey.babenko.rabota@gmail.com>
2025-04-19 19:06:30 +0500

    Load images and sounds

---
+++ src/main/java/com/wormfarm/gui/WormsProgram.java
@@ -0,0 +1,18 @@
+package com.wormfarm.gui;
+
+import com.wormfarm.gui.frame.MainApplicationFrame;
+import javax.swing.*;
+
+public class WormsProgram {
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        SwingUtilities.invokeLater(() -> {
+            MainApplicationFrame frame = new MainApplicationFrame();
+            frame.setVisible(true);
+        });
+    }
+}
